b2732aabe85bfca7b5a6fe13b9b986dbb1dfb1c7
Jonas <j.m.niederle@student.tue.nl>
2022-05-04 16:18:55 +0200

    recursively define calculation of V

--- Discrete-Simulations/environment.py
+++ Discrete-Simulations/environment.py
@@ -1,10 +1,12 @@
 import numpy as np
+import copy
 import random
-from robot_configs.value_iteration_robot import State
+from robot_configs.value_iteration_robot import State, get_possible_states
 
 SMALL_ENOUGH = 1e-3
-ALL_POSSIBLE_ACTIONS = ('U', 'D', 'L', 'R')
-
+#ALL_POSSIBLE_ACTIONS = ('U', 'D', 'L', 'R')
+orients = {'n': -3, 'e': -4, 's': -5, 'w': -6}
+dirs = {'n': (0, -1), 'e': (1, 0), 's': (0, 1), 'w': (-1, 0)}
 
 class Robot:
     def __init__(self, grid, pos, orientation, p_move=0, battery_drain_p=0, battery_drain_lam=0, vision=1):
@@ -25,7 +27,7 @@ class Robot:
         self.vision = vision
 
     def possible_tiles_after_move(self):
-        moves = list(self.dirs.values())
+        moves = list(dirs.values())
         # Fool the robot and show a death tile as normal (dirty)
         data = {}
         for i in range(self.vision):
@@ -148,22 +150,174 @@ def generate_grid(n_cols, n_rows):
     grid = Grid(n_cols, n_rows)
     return grid
 
+
+class State:
+    def __init__(self, grid, pos, orientation, p_move=0, battery_drain_p=0, battery_drain_lam=0):
+        # if grid.cells[pos[0], pos[1]] != 1:
+        #     raise ValueError
+        self.orientation = orientation
+        self.pos = pos
+        self.grid = grid
+        self.grid.cells[pos] = orients[self.orientation]
+        self.p_move = p_move
+        self.alive = True
+        self.battery_drain_p = battery_drain_p
+        self.battery_drain_lam = battery_drain_lam
+
+    def move(self):
+        # Can't move if we're dead now, can we?
+        if not self.alive:
+            return False
+        new_pos = tuple(np.array(self.pos) + dirs[self.orientation])
+        # Only move to non-blocked tiles:
+        if self.grid.cells[new_pos] >= 0:
+            tile_after_move = self.grid.cells[new_pos]
+            self.grid.cells[self.pos] = 0
+            self.grid.cells[new_pos] = orients[self.orientation]
+            self.pos = new_pos
+            # Death:
+            if tile_after_move == 3:
+                self.alive = False
+                return False
+            return True
+        else:
+            return False
+
+    def rotate(self, dir):
+        current = list(orients.keys()).index(self.orientation)
+        if dir == 'r':
+            self.orientation = list(orients.keys())[(current + 1) % 4]
+        elif dir == 'l':
+            self.orientation = list(orients.keys())[current - 1]
+        self.grid.cells[self.pos] = orients[self.orientation]
+
+
+
+    def get_possible_moves(self):
+        data = {}
+        moves = list(dirs.values())
+        i = 1
+        for move in moves:
+            to_check = tuple(np.array(self.pos) + (np.array(move) * (i + 1)))
+            if to_check[0] < self.grid.cells.shape[0] and to_check[1] < self.grid.cells.shape[1] and to_check[
+                0] >= 0 and to_check[1] >= 0:
+                data[tuple(np.array(move) * (i + 1))] = self.grid.cells[to_check]
+                # Show death tiles as dirty:
+                if data[tuple(np.array(move) * (i + 1))] == 3:
+                    data[tuple(np.array(move) * (i + 1))] = 1
+        return data
+
+    def get_reward(self):
+        reward_dict = {
+            -2: -2,
+            -1: -2,
+            0: -1,
+            1: 1,
+            2: 10,
+            3: -10
+        }
+        state_reward = reward_dict[self.grid.cells[self.pos]]
+
+        # modified from environment.py
+        # TODO: correct?
+        expected_drain = self.battery_drain_p * np.random.exponential(self.battery_drain_lam)
+
+        # reward is reward of moving to new state + expected battery drain (negative constant)
+        reward = state_reward - expected_drain
+
+        return reward
+
+    def get_neighbouring_states(self): # Checks neighboring state is not a wall/obstacle
+        moves = list(dirs.values())
+        states = {}
+        for move in moves:
+            new_move = tuple(np.array(self.pos) + np.array(move))
+            if new_move[0] < self.grid.cells.shape[0] and new_move[1] < self.grid.cells.shape[1] and \
+                    new_move[0] >= 0 and new_move[1] >= 0 and self.grid.cells[new_move] >=0:
+                new_pos = tuple(np.array(self.pos) + dirs[self.orientation])
+                new_state = copy.deepcopy(self)
+                # Find out how we should orient ourselves:
+                print(list(dirs.values()))
+                print(self.grid.cells[new_move])
+
+                new_orient = list(dirs.keys())[list(dirs.values()).index(self.grid.cells[new_move])]
+                # Orient ourselves towards the dirty tile:
+                while new_orient != new_state.orientation:
+                    # If we don't have the wanted orientation, rotate clockwise until we do:
+                    # print('Rotating right once.')
+                    new_state.rotate('r')
+
+                # Only move to non-blocked tiles:
+                if new_state.grid.cells[new_pos] >= 0:
+                    tile_after_move = new_state.grid.cells[new_pos]
+                    new_state.grid.cells[new_state.pos] = 0
+                    new_state.grid.cells[new_pos] = new_state.orients[new_state.orientation]
+                    new_state.pos = new_pos
+                    states.add(new_state)
+                    if tile_after_move == 3:
+                        self.alive = False
+        return states
+
+    def get_action_reward(self, action):
+        state_primes = self.get_neighbouring_states() # Excludes obstacles and walls
+        transitions = []
+        new_pos = tuple(np.array(self.pos) + self.dirs[action])
+
+        # Getting the transition probabilities
+        for state_prime in state_primes:
+            if state_prime.pos == new_pos:
+                prob = 1 - state_prime.p_move
+            elif state_prime.p_move == 0:
+                prob = 0
+            else:
+                prob = state_prime.p_move/(len(state_primes)-1)
+            transitions.append((prob, state_prime.get_reward(), state_prime))
+        return transitions
+
+
+def is_terminal(state):
+    # TODO: consider unreachable situations
+    g = state.grid.cells
+
+    return not np.isin(g, [1, 2]).any()
+
+
 def best_action_value(V, s, gamma):
     # finds the highest value action (max_a) from state s, returns the action and value
     best_a = None
     best_value = float('-inf')
 
     # loop through all possible actions to find the best current action
-    for a in ALL_POSSIBLE_ACTIONS:
-        transititions = s.get_transition_probs(a)
-        sum = 0
-        for (prob, r, state_prime) in transititions:
-            sum += prob * (r + (gamma * V[state_prime]))
-        v = sum
+    for a in dirs.keys():
+        state_primes = s.get_action_reward(a)
+        sum_a = 0
+        for (prob, r, state_prime) in state_primes:
+            if not (state_prime.grid.cells, state_prime.pos) in V:
+                V[(state_prime.grid.cells, state_prime.pos)] = 0
+
+            sum_a += prob * (r + (gamma * V[(state_prime.grid.cells, state_prime.pos)]))
+
+        v = sum_a
         if v > best_value:
             best_value = v
             best_a = a
     return best_a, best_value
+
+
+def evaluate_state(state, V, gamma):
+
+    if is_terminal(state):
+        return V
+
+    best_a, best_val = best_action_value(V, state, gamma)
+
+    V[(state.grid.cells, state.pos)] = best_val
+
+    for new_state in state.get_neighbouring_states():
+        if not (new_state.grid.cells, new_state.pos) in V:
+            V = evaluate_state(new_state, V, gamma)
+
+    return V
 
 
 class SmartRobot(Robot):
@@ -185,53 +339,49 @@ class SmartRobot(Robot):
         self.vision = vision
         self.gamma = gamma
         self.V = self.calculate_values()
-        self.policy = self.calculate_policy()
+        # self.policy = self.calculate_policy()
 
 
     def calculate_values(self):
         current_state = State(self.grid, self.pos, self.orientation, self.p_move, self.battery_drain_p,
                               self.battery_drain_lam)
+        V = {}
+        return evaluate_state(current_state, V, self.gamma)
 
         #initialize V(s)
-        V = {}
-        possible_states = current_state.get_possible_states()
+
+        possible_states = get_possible_states(current_state)
         print("robot.states:", possible_states)
         for s in possible_states:
             V[s] = 0
         #repeat until convergence
         # V[s] = max[a]{ sum[s',r] { p(s',r|s,a)[r + gamma*V[s']] } }
-        while True:
+        while biggest_change > SMALL_ENOUGH:
             # biggest_change is referred to by the mathematical symbol delta in equations
             biggest_change = 0
+
+            current_state
+
             for s in possible_states:
                 old_v = V[s]
                 _, new_v = best_action_value(V, s, self.gamma)
                 V[s] = new_v
                 biggest_change = max(biggest_change, np.abs(old_v - new_v))
 
-            if biggest_change < SMALL_ENOUGH:
-                break
         return V
 
-    def initialize_random_policy(self):
-        # policy is a lookup table for state -> action
-        # we'll randomly choose an action and update as we learn
-        policy = {}
-        for s in self.grid.non_terminal_states():
-            policy[s] = np.random.choice(ALL_POSSIBLE_ACTIONS)
-        return policy
 
-    def calculate_policy(self):
-        current_state = State(self.grid, self.pos, self.orientation, self.p_move, self.battery_drain_p,
-                              self.battery_drain_lam)
-        possible_states = current_state.get_possible_states()
-        policy = {}
-        # find a policy that leads to optimal value function
-        for s in possible_states:
-            # loop through all possible actions to find the best current action
-            best_a, _ = best_action_value(self.V, s)
-            policy[(s.grid.cells, s.pos)] = best_a
-        return policy
+    # def calculate_policy(self):
+    #     current_state = State(self.grid, self.pos, self.orientation, self.p_move, self.battery_drain_p,
+    #                           self.battery_drain_lam)
+    #     possible_states = current_state.get_possible_states()
+    #     policy = {}
+    #     # find a policy that leads to optimal value function
+    #     for s in possible_states:
+    #         # loop through all possible actions to find the best current action
+    #         best_a, _ = best_action_value(self.V, s)
+    #         policy[(s.grid.cells, s.pos)] = best_a
+    #     return policy
 
 #{(grid, pos): best_a, }
 
